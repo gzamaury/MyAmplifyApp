8c0782171ae80920a2adff8e898f3ceb02c0a18d
AmauryGz <gzamaury@hotmail.com>
2021-05-05 21:04:44 -0500

    Amplify Libraries iOS Tutorial - Create your application
    https://docs.amplify.aws/lib/q/platform/ios

---
+++ MyAmplifyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAmplifyApp
+//
+//  Created by MacGza on 05/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
