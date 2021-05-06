662882fc626a0306a74e2e7d0056dd3be76a2dd0
AmauryGz <gzamaury@hotmail.com>
2021-05-06 18:00:05 -0500

    Initialize Amplify in the application

--- MyAmplifyApp/AppDelegate.swift
+++ MyAmplifyApp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Amplify
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        do {
+            Amplify.Logging.logLevel = .verbose
+            try Amplify.configure()
+        } catch {
+            print("An error occurred setting up Amplify: \(error)")
+        }
+        
         return true
     }
 
