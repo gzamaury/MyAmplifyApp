53307434d4b41599addc2e543d087aceae89088e
AmauryGz <gzamaury@hotmail.com>
2021-05-07 22:23:45 -0500

    Record events

--- MyAmplifyApp/ViewController.swift
+++ MyAmplifyApp/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Amplify
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        recordEvents()
     }
 
 
 }
 
+
+// MARK: ViewController Extension
+extension ViewController {
+    func recordEvents() {
+        let properties: AnalyticsProperties = [
+            "eventPropertyStringKey": "eventPropertyStringValue",
+            "eventPropertyIntKey": 123,
+            "eventPropertyDoubleKey": 12.34,
+            "eventPropertyBoolKey": true
+        ]
+        let event = BasicAnalyticsEvent(name: "eventName", properties: properties)
+        
+        Amplify.Analytics.record(event: event)
+    }
+}
